5959446271d7ddaecacaa2cdbd1dbf21876c3c0c
Gabriel Gomez Palomäki <a20gabpa@student.his.se>
2024-05-19 22:33:44 +0200

    Set 'imageView' by external library
    Using Picasso, update the image. The link used return a random image of a certain time every it's accessed.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* =============== VARIABLES =============== */
+    private ImageView imageView;
+    // Returns a random image every time the linked is accessed
+    private String randomImage = "https://picsum.photos/600/900";
+    /* ========================================= */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get reference to image
+        imageView = findViewById(R.id.imageView);
+        // Using Picasso, update the image
+        Picasso.with(this).load(randomImage).into(imageView);
     }
 }
